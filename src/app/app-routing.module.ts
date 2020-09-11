a9cd3cdbedea799e8a49d26caea7a3aba0ddbdc5
RyenToretto <18273727925@163.com>
2020-09-11 18:38:39 +0800

    routing 路由组件

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SunTribeComponent } from './sun-tribe/sun-tribe.component';
 
+// 告诉路由器, 当 用户单击链接 或 将 URL 粘贴进浏览器地址栏 时，要显示哪个视图
 const routes: Routes = [
-    { path: 'sunTribe', component: SunTribeComponent }
+    { path: 'suntribe', component: SunTribeComponent }
 ];
 
 @NgModule({
+    // 在应用的顶层配置这个路由器。
+    // forRoot() 方法会提供路由所需的服务提供者和指令，还会基于浏览器的当前 URL 执行首次导航
     imports: [RouterModule.forRoot(routes)],
+
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
