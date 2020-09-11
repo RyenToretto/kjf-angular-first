6df75df2996c42704167dab5eeccf15119b8cf5f
RyenToretto <18273727925@163.com>
2020-09-11 17:33:27 +0800

    rxjs 中的 Observable 类，实例对象 进行订阅，异步请求数据

---
+++ src/app/hero.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+import {Observable, of} from "rxjs";
+
+import {Hero} from "./declares/hero";
+
+import {HEROES} from "./mock-data/mock-heroes";
+
+/**
+ 可以从任何地方获取数据：Web 服务、本地存储（LocalStorage）或一个模拟的数据源
+ */
+@Injectable({
+    providedIn: 'root' // 用 根注入器 将 你的服务 注册成为提供者
+})
+export class HeroService {
+
+    constructor() { }
+
+    /****
+     * 应用从远端服务器获取英雄数据，天生就是异步操作
+     * HeroService 必须等服务器给出响应， 而 getHeroes() 不能立即返回英雄数据， 浏览器也不会在该服务等待期间停止响应
+     * 所以 HeroService.getHeroes() 必须具有某种形式的异步函数： 如 rxjs 中 Observable 类的 实例对象
+     *
+     * 在最终会使用 Angular 的 HttpClient.get 方法来 ajax 获取英雄数据
+     * 而 HttpClient.get() 会返回 Observable
+     *
+     这里使用 RxJS 的 of() 函数来模拟从服务器返回数据
+     ****/
+    requestHeroes(): Observable<Hero[]> {
+        return of(HEROES);
+    }
+}
--- src/app/sun-tribe/sun-tribe.component.ts
+++ src/app/sun-tribe/sun-tribe.component.ts
@@ -1,25 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
-import {HEROES} from '../mock-data/mock-heroes';
 import {Hero} from '../declares/hero';
+import {HeroService} from "../hero.service";
 
 @Component({
     selector: 'app-sun-tribe',
     templateUrl: './sun-tribe.component.html',
     styleUrls: ['./sun-tribe.component.scss']
 })
-export class SunTribeComponent implements OnInit {
+export class SunTribeComponent implements OnInit { // Angular 会在构造出 SunTribeComponent 的实例之后的某个合适的时机调用 ngOnInit()
     selectedHero: Hero = {};
 
     hero: Hero = {
         id: 1,
         name: 'KouJianFeng'
     };
-    heroes: Hero[] = HEROES;
+    heroes: Hero[];
 
-    constructor() { }
+    // 1. 声明了一个私有 heroService 属性
+    // 2. 把它标记为一个 HeroService 的注入点
+    // 当 Angular 创建 SunTribeComponent 时，依赖注入系统就会把这个 heroService 参数设置为 HeroService 的单例对象
+    constructor(private heroService: HeroService) { }
 
     ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    /****
+     * 等待 Observable 发出这个英雄数组，这可能立即发生，也可能会在几分钟之后。
+     * 然后 subscribe() 方法把这个英雄数组传给这个回调函数，
+     * 该函数把英雄数组赋值给组件的 heroes 属性。
+     *
+     使用这种异步方式，当 HeroService 从远端服务器获取英雄数据时，就可以工作了
+     ****/
+    getHeroes(): void {
+        this.heroService.requestHeroes()
+            .subscribe(heroes => this.heroes = heroes);
     }
 
     onSelect(hero: Hero): void {
