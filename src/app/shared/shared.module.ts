dd9a45d0b4890aa08d841aa427f7f81c398ce38d
RyenToretto <18273727925@163.com>
2020-09-16 14:11:51 +0800

    AppSharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { SunTribeComponent } from '../components/sun-tribe/sun-tribe.component';
+import { MoonTribeComponent } from '../components/moon-tribe/moon-tribe.component';
+import { HeroDetailComponent } from '../components/hero-detail/hero-detail.component';
+import { HeroMessagesComponent } from '../components/hero-messages/hero-messages.component';
+
+const SHARED_COMPONENT = [SunTribeComponent, MoonTribeComponent, HeroDetailComponent, HeroMessagesComponent];
 
 @NgModule({
-    imports: [CommonModule, FormsModule, RouterModule, BrowserAnimationsModule],
-    exports: [CommonModule, FormsModule, RouterModule],
-    declarations: [],
+    declarations: [...SHARED_COMPONENT],
+    imports: [CommonModule, FormsModule, RouterModule],
+    exports: [CommonModule, FormsModule, RouterModule, ...SHARED_COMPONENT],
     providers: [],
 })
 export class AppSharedModule {}
