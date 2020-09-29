aaefcd038822cfc3a83b6d365b46276cf806438a
RyenToretto <18273727925@163.com>
2020-09-29 17:49:11 +0800

    FormControl, FormGroup 表单

--- src/app/components/name-editor/name-editor.component.ts
+++ src/app/components/name-editor/name-editor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
     selector: 'app-name-editor',
@@ -8,10 +8,19 @@ import { FormControl } from '@angular/forms';
 export class NameEditorComponent implements OnInit {
 
     fcName = new FormControl();
+    formRegister = new FormGroup({
+        username: new FormControl('newUser'),
+        password: new FormControl(''),
+    });
 
     constructor() { }
 
     ngOnInit(): void {
     }
 
+    fcUpdateName() {
+        this.fcName.setValue('formControl 上的setValue() 方法更新表单项~');
+    }
+
+    handleSubmit() {}
 }
